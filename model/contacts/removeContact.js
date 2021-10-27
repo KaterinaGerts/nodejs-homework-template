991c8eada34fa40c9dc73a9a7386af12c380d1d8
KaterinaGerts <75572090+KaterinaGerts@users.noreply.github.com>
2021-10-27 10:13:03 +0300

    внесла корректировки в функцию удаления и обновления

--- model/contacts/removeContact.js
+++ model/contacts/removeContact.js
@@ -1,25 +1,20 @@
-/* eslint-disable no-useless-catch */
 const fs = require('fs/promises')
 const contactsPath = require('./contactPath')
 const listContacts = require('./listContacts')
 
 const removeContact = async contactId => {
-  try {
-    const contacts = await listContacts()
-    const idx = contacts.findIndex(contact => contact.id === Number(contactId))
-    if (idx === -1) {
-      return null
-    }
-    const newContacts = contacts.filter(
-      contact => contact.id !== Number(contactId),
-    )
-    const contactsString = JSON.stringify(newContacts)
-    await fs.writeFile(contactsPath, contactsString)
-    console.table(newContacts)
-    return 'Success remove!'
-  } catch (error) {
-    throw error
+  const contacts = await listContacts()
+  const idx = contacts.findIndex(contact => contact.id === contactId)
+  if (idx === -1) {
+    return null
   }
+  const newContacts = contacts.filter(
+    contact => contact.id !== contactId,
+  )
+  const contactsString = JSON.stringify(newContacts)
+  await fs.writeFile(contactsPath, contactsString)
+  console.table(newContacts)
+  return 'Success remove!'
 }
 
 module.exports = removeContact
