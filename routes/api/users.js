86d2c2382fd9b9e44c000ea4f49581c842534b4e
KaterinaGerts <75572090+KaterinaGerts@users.noreply.github.com>
2021-11-15 11:29:08 +0300

    сократила код в раутах

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,14 +1,12 @@
 const express = require('express')
 
 const joiUserSchema = require('../../models/user')
-const validation = require('../../middlewares/validation')
-const { auth: ctrl } = require('../../controllers')
+const { validation } = require('../../middlewares')
+const ctrl = require('../../controllers/auth')
 
 const router = express.Router()
 
-const userValidationMiddleware = validation(joiUserSchema)
-
-router.post('/signup', userValidationMiddleware, ctrl.signUp)
+router.post('/signup', validation(joiUserSchema), ctrl.signUp)
 
 // router.post('/login', ctrl.login)
 
